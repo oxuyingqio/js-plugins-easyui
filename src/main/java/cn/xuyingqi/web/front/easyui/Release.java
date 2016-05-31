39a51fe538d056edfc0237126478ca73d70f0b6c
oxuyingqio <oxuyingqio@qq.com>
2016-05-31 16:11:00 +0800

    X

--- src/main/java/cn/xuyingqi/web/front/easyui/Release.java
+++ src/main/java/cn/xuyingqi/web/front/easyui/Release.java
@@ -9,7 +9,7 @@ import cn.xuyingqi.util.tool.operatefile.impl.HandleSpecifyFormatFile;
 import cn.xuyingqi.util.util.FileUtils;
 
 /**
- * 合并前台组件JS为一个文�?
+ * 合并前台组件JS为一个文件
  * 
  * @author XuYQ
  *
@@ -23,43 +23,15 @@ public class Release {
 		String projectPath = System.getProperty("user.dir");
 		// 前台核心公共路径
 		String jsCommonPath = "/src/main/resources/web/core/";
-		// 前台核心包路径集�?
-		String[] packagePaths = { "package.js", "component" };
+		// 前台核心包路径集合
+		String[] packagePaths = { "package.js", "easyui" };
 
 		// 文件类型
 		String fileType = ".js";
 		// 生成文件路径
-		File coreFile = new File("D:/Users/XuYQ/Desktop/component.js");
-		// File coreFile = new File("D:/用户目录/我的桌面/component.js");
+		File coreFile = new File("D:/Users/XuYQ/Desktop/easyui.js");
 
-		// 若文件存�?,则删�?
-		if (coreFile.exists()) {
-			coreFile.delete();
-		}
-
-		for (int i = 0; i < packagePaths.length; i++) {
-			FileUtils.recursionFile(new File(projectPath + jsCommonPath + packagePaths[i]),
-					new HandleSpecifyFormatFile(fileType, new CopyFileContent2SpecifiedFile(coreFile)));
-		}
-
-		logger.debug("操作结束");
-	}
-
-	public static void releaseCss() {
-		// 项目路径
-		String projectPath = System.getProperty("user.dir");
-		// 前台核心公共路径
-		String jsCommonPath = "/src/main/resources/web/core/";
-		// 前台核心包路径集�?
-		String[] packagePaths = { "component" };
-
-		// 文件类型
-		String fileType = ".css";
-		// 生成文件路径
-		File coreFile = new File("D:/Users/XuYQ/Desktop/component.css");
-		// File coreFile = new File("D:/用户目录/我的桌面/component.css");
-
-		// 若文件存�?,则删�?
+		// 若文件存在,则删除
 		if (coreFile.exists()) {
 			coreFile.delete();
 		}
@@ -74,6 +46,5 @@ public class Release {
 
 	public static void main(String[] args) {
 		Release.releaseJs();
-		Release.releaseCss();
 	}
 }
