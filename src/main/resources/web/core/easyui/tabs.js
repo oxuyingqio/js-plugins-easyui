763287d026c40d3d1798800db22f26062dfa4a89
oxuyingqio <oxuyingqio@qq.com>
2016-05-31 16:08:51 +0800

    X

---
+++ src/main/resources/web/core/easyui/tabs.js
@@ -0,0 +1,16 @@
+$.extend($.fn.tabs.methods, {
+	// 双击事件
+	onDblClick : function(jq, caller) {
+		return jq.each(function() {
+			var that = this;
+			$(this).children("div.tabs-header").find("ul.tabs").undelegate("li", "dblclick.tabs").delegate("li",
+					"dblclick.tabs", function(e) {
+						if (caller && typeof (caller) == "function") {
+							var title = $(this).text();
+							var index = $(that).tabs("getTabIndex", $(that).tabs("getTab", title));
+							caller(index, title);
+						}
+					});
+		});
+	}
+});
