141152122911267697e11e79f70671c38a9c3a69
oxuyingqio <oxuyingqio@qq.com>
2016-08-29 15:05:08 +0800

    X

---
+++ src/main/resources/web/core/html/easyui/base/Tooltip.js
@@ -0,0 +1,146 @@
+/**
+ * @name Tooltip
+ * @package core.html.easyui.base
+ * @desc 工具提示模板
+ * @type 类
+ * 
+ * @date 2016年8月29日 15:03:57
+ */
+
+core.html.easyui.base.Tooltip = (function() {
+
+	/**
+	 * 对象个数
+	 */
+	var count = 0;
+
+	/**
+	 * 构造函数
+	 * 
+	 * @param id{String}
+	 *            ID
+	 */
+	var Constructor = function(_id) {
+
+		// 对象个数+1
+		count++;
+
+		/**
+		 * 属性
+		 */
+		/**
+		 * ID
+		 */
+		var id = _id || "coreHtmlEasyuiLayoutPanel" + count;
+
+		/**
+		 * 获取/设置 id
+		 * 
+		 * @param id
+		 */
+		this.id = function() {
+
+			switch (arguments.length) {
+			case 0:
+				return id;
+			default:
+				id = arguments[0];
+				return this;
+			}
+		};
+	};
+
+	/**
+	 * 初始化组件模板
+	 * 
+	 * @returns {core.html.easyui.layout.Panel}
+	 */
+	Constructor.prototype.init = function() {
+
+		// 获取jQuery对象
+		var $jQuery = $("#" + this.id());
+		// 判断ID是否存在
+		if ($jQuery.length === 0) {
+			new core.lang.Exception(this, "core.html.easyui.layout.Panel", "参数异常", "div(id:" + this.id() + ")不存在.");
+		}
+		// 清空内容
+		$jQuery.empty();
+
+		// 参数配置
+		$jQuery.panel({
+			// 属性
+			id : this.id(),
+			title : this.title(),
+			iconCls : this.iconCls(),
+			width : this.width(),
+			height : this.height(),
+			left : this.left(),
+			top : this.top(),
+			cls : this.cls(),
+			headerCls : this.headerCls(),
+			bodyCls : this.bodyCls(),
+			style : this.style(),
+			fit : this.fit(),
+			border : this.border(),
+			doSize : this.doSize(),
+			noheader : this.noheader(),
+			content : this.content(),
+			collapsible : this.collapsible(),
+			minimizable : this.minimizable(),
+			maximizable : this.maximizable(),
+			closable : this.closable(),
+			tools : this.tools(),
+			header : this.header(),
+			footer : this.footer(),
+			openAnimation : this.openAnimation(),
+			openDuration : this.openDuration(),
+			closeAnimation : this.closeAnimation(),
+			closeDuration : this.closeDuration(),
+			collapsed : this.collapsed(),
+			minimized : this.minimized(),
+			maximized : this.maximized(),
+			closed : this.closed(),
+			href : this.href(),
+			cache : this.cache(),
+			loadingMessage : this.loadingMessage(),
+			extractor : this.extractor(),
+			method : this.method(),
+			queryParams : this.queryParams(),
+			loader : this.loader(),
+
+			// 事件
+			onBeforeLoad : this.onBeforeLoad(),
+			onLoad : this.onLoad(),
+			onLoadError : this.onLoadError(),
+			onBeforeOpen : this.onBeforeOpen(),
+			onOpen : this.onOpen(),
+			onBeforeClose : this.onBeforeClose(),
+			onClose : this.onClose(),
+			onBeforeDestroy : this.onBeforeDestroy(),
+			onDestroy : this.onDestroy(),
+			onBeforeCollapse : this.onBeforeCollapse(),
+			onCollapse : this.onCollapse(),
+			onBeforeExpand : this.onBeforeExpand(),
+			onExpand : this.onExpand(),
+			onResize : this.onResize(),
+			onMove : this.onMove(),
+			onMaximize : this.onMaximize(),
+			onRestore : this.onRestore(),
+			onMinimize : this.onMinimize()
+		});
+
+		return this;
+	};
+
+	/**
+	 * 
+	 * @returns
+	 */
+	Constructor.prototype.options = function() {
+
+		return $("#" + this.id()).panel("options");
+	};
+
+	// 返回构造函数
+	return Constructor;
+})();
--- src/main/resources/web/core/package.js
+++ src/main/resources/web/core/package.js
@@ -5,8 +5,17 @@
 // HTML EasyUI扩展包
 core.html.easyui = {
 
+	// 基础包
+	base : {},
+
+	// 按钮包
+	button : {},
+
 	// 列表包
 	datagrid : {},
+
+	// 表单包
+	form : {},
 
 	// 布局包
 	layout : {},
@@ -14,6 +23,3 @@ core.html.easyui = {
 	// 窗口包
 	window : {}
 };
-
-// HTML EasyUI元素包
-core.html.element.easyui = {};
